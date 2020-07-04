0c3ffda3e2249c3d35ded577957aef8418bd898f
Pravin S M <65936685+PravinSwaranaMariappan@users.noreply.github.com>
2020-07-04 09:37:17 +0530

    Updates

---
+++ precache-manifest.e3ca6f355a5505bc54ea9bf3d8823a14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944664f0b91a62b1ffe2e95494727789",
+    "url": "/Corona_Results/index.html"
+  },
+  {
+    "revision": "9b5ec4f73c8ed698d11e",
+    "url": "/Corona_Results/static/css/main.ad75f3fd.chunk.css"
+  },
+  {
+    "revision": "77f39e9648df11e58b41",
+    "url": "/Corona_Results/static/js/2.fc655bf1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona_Results/static/js/2.fc655bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5ec4f73c8ed698d11e",
+    "url": "/Corona_Results/static/js/main.7999b488.chunk.js"
+  },
+  {
+    "revision": "e631ed7dfba3693c001e",
+    "url": "/Corona_Results/static/js/runtime-main.82dcc829.js"
+  },
+  {
+    "revision": "6beaa9f55987c410740e2d03b213442d",
+    "url": "/Corona_Results/static/media/image.6beaa9f5.png"
+  }
+]);
